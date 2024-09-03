6939e8e58c84dc7b526560f027c4a96969f070dd
Sukh-Virk <sukhmanqe@gmail.com>
2024-09-02 22:33:37 -0700

    Add files via upload

---
+++ list.js
@@ -0,0 +1,109 @@
+const hangmanWords = [
+    {
+        word: "basketball",
+        hint: "Sport Sukhman likes to play."
+    },
+    {
+        word: "mma",
+        hint: "Sukhman's favorite sport to watch."
+    },
+    {
+        word: "nebula",
+        hint: "A cloud of gas and dust in space, often the birthplace of stars."
+    },
+    {
+        word: "voyage",
+        hint: "A long journey, especially by sea."
+    },
+    {
+        word: "cryptic",
+        hint: "Having a hidden or mysterious meaning."
+    },
+    {
+        word: "kodak",
+        hint: "No flocking."
+    },
+    {
+        word: "phantom",
+        hint: "A ghostly figure."
+    },
+    {
+        word: "labyrinth",
+        hint: "A complicated maze or network of paths."
+    },
+    {
+        word: "serendipity",
+        hint: "The occurrence of events by chance in a happy or beneficial way."
+    },
+    {
+        word: "ephemeral",
+        hint: "Lasting for a very short time; fleeting."
+    },
+    {
+        word: "zephyr",
+        hint: "A gentle, mild breeze."
+    },
+    {
+        word: "mellifluous",
+        hint: "A sound that is sweet and smooth, pleasing to hear."
+    },
+    {
+        word: "limerence",
+        hint: "The state of being infatuated with another person."
+    },
+    {
+        word: "solstice",
+        hint: "The time when the sun is at its greatest distance from the equator."
+    },
+    {
+        word: "quixotic",
+        hint: "Extremely idealistic; unrealistic and impractical."
+    },
+    {
+        word: "aurelia",
+        hint: "A golden or gilded object, often associated with the image of a halo."
+    },
+    {
+        word: "Black",
+        hint: "Sukhman's Fav Color"
+    },
+    {
+        word: "ethereal",
+        hint: "Extremely delicate and light in a way that seems too perfect for this world."
+    },
+    {
+        word: "nocturnal",
+        hint: "Active during the night."
+    },
+    {
+        word: "resonance",
+        hint: "The quality of a sound being deep, full, and reverberating."
+    },
+    {
+        word: "quintessential",
+        hint: "Representing the most perfect or typical example of a quality or class."
+    },
+    {
+        word: "Sukhmans Favorite animal",
+        hint: "Tiger."
+    },
+    {
+        word: "lucid",
+        hint: "Expressed clearly; easy to understand."
+    },
+    {
+        word: "sapphire",
+        hint: "A precious gemstone, typically blue, but can be other colors as well."
+    },
+
+    {
+        word: "loser",
+        hint: "what you are."
+    },
+
+    {
+        word: "Holiday",
+        hint: "halloween"
+    }
+
+];
